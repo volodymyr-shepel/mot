de36d602349e1a3c4a53bd93dab2b72823544d8c
Volodymyr Shepel <v.shepel158@gmail.com>
2024-01-05 12:05:58 +0100

    Small modifications in search screen

--- frontend/mobile/lib/screens/search_screen/child_categories_screen.dart
+++ frontend/mobile/lib/screens/search_screen/child_categories_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:mot/helper/keyboard.dart';
 import 'package:mot/models/category.dart';
 import 'package:mot/screens/search_screen/components/category_widget.dart';
+import 'package:mot/screens/search_screen/components/search_field.dart';
 
 class ChildCategoriesScreen extends StatelessWidget {
   final Category parentCategory;
@@ -11,12 +13,29 @@ class ChildCategoriesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(parentCategory.name),
+      leading: IconButton(
+        onPressed: () {
+          KeyboardUtil.hideKeyboard(context);
+          Navigator.pop(context);
+        },
+        icon: const Icon(Icons.arrow_back_ios),
       ),
+      automaticallyImplyLeading: false,
+      title: Text(
+        parentCategory.name,
+        style: TextStyle(fontSize: 20.0, color: Colors.white),
+      ),
+    ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.symmetric(vertical: 20),
         child: Column(
           children: [
+            const SearchField(
+            hintText: "Czego szukasz?",
+            keyboardType: TextInputType.text, // Set the keyboard type,
+            svgIconPath: 'assets/icons/Search Icon.svg',
+          ),
+          const SizedBox(height: 15),
             if (parentCategory.childCategories != null)
               for (var childCategory in parentCategory.childCategories!)
                 Builder(
